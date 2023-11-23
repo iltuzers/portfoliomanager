93814631eea882719acf1d770a551f8cf33bbb1b
iltuzers <iltuzers@gmail.com>
2023-11-23 05:59:27 -0500

    Create Authentication Templates

--- portfoliomanager/urls.py
+++ portfoliomanager/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
-    path('', RedirectView.as_view(url='porfolio/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='portfolio/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
