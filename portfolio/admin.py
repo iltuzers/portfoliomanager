47fa6a5f90bfd809c73ea65046da10c993fd7334
iltuzers <iltuzers@gmail.com>
2023-11-24 16:03:15 -0500

    Add Custom  M2M through Models - StockQuantity and OptionQuantity

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Stock, Option, Portfolio
+from .models import Stock, Option, Portfolio, StockQuantity, OptionQuantity
 
 admin.site.register(Stock)
 admin.site.register(Option)
 admin.site.register(Portfolio)
+admin.site.register(StockQuantity)
+admin.site.register(OptionQuantity)
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -7,8 +7,6 @@ from datetime import date, timedelta
 class Stock(models.Model):
     symbol = models.CharField(max_length=32, unique=True)
 
-     # negative if short, positive if long
-    quantity = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['symbol']
@@ -27,9 +25,6 @@ class Option(models.Model):
     underlying = models.ForeignKey(Stock, on_delete=models.CASCADE)
     expiration_date = models.DateField()
     strike = models.DecimalField(max_digits=10, decimal_places=2)
-
-    # negative if short, otherwise positive
-    quantity = models.IntegerField(default=0)
 
     OPTION_TYPE = (
         ('C', 'Call'),
@@ -66,34 +61,36 @@ class Option(models.Model):
 
 
 class Portfolio(models.Model):
-    INSTRUMENT_TYPE = (
-        ('O', 'Option'),
-        ('S', 'Stock'),
-    )
-    instrument_type = models.CharField(
-        max_length = 1,
-        choices = INSTRUMENT_TYPE,
-        help_text = 'Stock or Option Type',
-    )
+    
     name = models.CharField(max_length=32, default="Default") # may change this to username
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    stock = models.ManyToManyField(Stock, blank=True)
-    option = models.ManyToManyField(Option, blank=True)
-
+    stock = models.ManyToManyField(Stock, blank=True, through="StockQuantity")
+    option = models.ManyToManyField(Option, blank=True, through="OptionQuantity")
    
-    
     class Meta:
         ordering = ['owner']
         unique_together = [['name', 'owner']]
-
-    
-    
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('portfolio-detail', args=[str(self.id)])
-    
-    
+
+
+class StockQuantity(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.portfolio}_{self.stock}_{self.quantity}"
+
+class OptionQuantity(models.Model):
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.portfolio}_{self.option}_{self.quantity}"
     
